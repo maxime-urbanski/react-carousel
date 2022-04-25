0bee1581e8ec990680f216521ed7e90770b3c675
Urbanski Maxime <maxime@les-tilleuls.coop>
2022-04-25 10:31:31 +0200

    Infinite

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,26 @@ import {useEffect, useState} from "react";
 import type { NextPage } from 'next'
 import Axios from "axios";
 import Carousel from "@/components/carousel/Carousel";
+import {Logger} from "sass";
 
 const Home: NextPage = () => {
 
   const [img, setImg] = useState<Array<string>>([])
 
-  let imgUrl: Array<string> = []
   useEffect( () => {
+    let test: Array<string> = []
     const fetchImg = async () => {
       const req = await  Axios.get('https://picsum.photos/v2/list?limit=7');
       const res = req.data;
-      imgUrl = [...res, res];
+      const getImgUrl = res.map(({download_url}) => download_url);
+      setImg(getImgUrl);
     }
-    fetchImg()
+    fetchImg();
   }, [])
 
-
-  imgUrl.forEach((element) => setImg(element.download_url))
   return (
     <div className="container">
-     <Carousel array={img} elementVisible={1} elementToSlide={1} />
+     <Carousel array={img} elementVisible={1} elementToSlide={1} setImg={setImg} />
     </div>
   )
 }
--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,28 +1,48 @@
-import {useEffect, useRef, useState} from "react";
+import React, {SetStateAction, useMemo, useState} from "react";
 
 interface CarouselProps {
   array: Array<string>
   elementVisible: number
-  elementToSlide: number
+  elementToSlide: number;
+  setImg: React.Dispatch<SetStateAction<Array<string>>>
 }
 
 
-const Carousel = ({array, elementVisible, elementToSlide}: CarouselProps):JSX.Element => {
+const Carousel = ({array, elementVisible, elementToSlide, setImg}: CarouselProps):JSX.Element => {
 
   const [index, setIndex] = useState<number>(0)
+
+  const indexMemo = useMemo(() => index, [index])
+
   const prev = () => {
-    if (index < 0) {
-      setIndex(0)
-    }
+    let infinite = [...array.slice(-elementToSlide), ...array].slice(0,array.length)
+
+    setImg(infinite)
+
     setIndex(index - 1)
+
+    if (index <= 0) {
+      setIndex(array.length - 1)
+    }
+
+    console.log(index)
+
   }
   const next = () => {
+    let infinite = [...array, ...array.slice(0,elementToSlide)].slice(-array.length)
+    setImg(infinite)
     setIndex(index + 1)
+    if (index > array.length - 2) {
+      setIndex(0)
+    }
+    console.log(array.slice(-array.length))
+
+
   }
 
-  const test = elementVisible / elementToSlide
+  const ratio = elementVisible / elementToSlide
 
-  const translate = (100 / test) * index + "%";
+  const translate = (100 / ratio) * indexMemo + "%";
 
   const widthVisible = 100 / elementVisible + "%"
 
@@ -30,7 +50,7 @@ const Carousel = ({array, elementVisible, elementToSlide}: CarouselProps):JSX.El
     <div style={{display: "flex", flexDirection:'column'}}>
       <div className="carousel">
         <button className="button prev" onClick={prev}>Prev</button>
-        <div className="carousel-container" style={{transform: `translateX(-${translate})`}}>
+        <div className="carousel-container" style={{transform : `translateX(-${translate})`}}>
           {array.map( element =>
             <div
               className="carousel-item"
